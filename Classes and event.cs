59a2697f6af1d92064201747d26de34d76df268a
Daryayan <daryasa1991@mail.ru>
2019-02-14 22:53:41 +0300

    pract

---
+++ Classes and event.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Practice
+{
+
+    public class Memes
+    {
+        public int HeheScale;
+        public int HaHaScale;
+        volatile bool IsItAGoodMeme;
+        public void Comment()
+        {
+            Console.WriteLine("is there a complete video?");
+        }
+    }
+    public class Doggy : Memes
+    {
+        public long Cute { get; set; }
+        public Doggy(int hehe, int haha)
+        {
+            HeheScale = hehe;
+            HaHaScale = haha;
+        }
+    }
+
+    internal class NewMemeEventArgs : EventArgs
+    {
+        private readonly string typeOfMemes, MemeFrom;
+
+        public NewMemeEventArgs(string type, string mFrom)
+        {
+            typeOfMemes = type;
+            MemeFrom = mFrom;
+        }
+
+        public string From { get { return MemeFrom; } }
+        public string Meme { get { return "Ha-ha"; } }
+    }
+
+
+}
