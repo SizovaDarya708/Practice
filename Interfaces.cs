6f57aa0dfea314e84679f4605e24880843a8931f
Daryayan <daryasa1991@mail.ru>
2019-02-15 21:54:56 +0300

    task

---
+++ Interfaces.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Practice
+{
+    internal class Size
+    {
+        int width;
+        int height;
+        public Size(int h, int w)
+        {
+            height = h;
+            width = w;
+        }
+    }
+
+    interface IArt
+    {
+        long Price { get; }
+        Size Size(int height, int width);
+        string Name { get; set; }
+        string Painter { get; set; }
+    }
+    interface IClient
+    {
+        string Name { get; }
+    }
+
+    internal class Painting : IArt, IClient
+    {
+        public long Price { get; }
+        public string Name { get; set; }
+        public string Painter { get; set; }
+        public Size Size(int height, int width)
+        {
+            throw new Exception();
+        }
+        string ClientName { get; }
+    }
+
+    class Proram
+    { 
+
+
+    }
+}
---
+++ Practice.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Practice
+{
+    public class Memes
+    {
+        public int HeheScale;
+        public int HaHaScale;
+        volatile bool IsItAGoodMeme;
+        delegate void Comments();
+        public void Comment(string memeType)
+        {
+            Comments wat;
+            Console.WriteLine("is there a complete video?");
+            if (DateTime.Now.Year < 2000)
+            {
+                wat = ISeeThisMeme;
+            }
+            else wat = IsThisANewJoke;
+        }
+  
+        private static void ISeeThisMeme()
+        {
+            Console.WriteLine("Yeah that's funny");
+        }
+        private static void IsThisANewJoke()
+        {
+            Console.WriteLine("I see it for the first time");
+        }
+    }
+    public class Doggy : Memes
+    {
+        public long Cute
+        {
+            get { return 333333; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("dude, dogs are nicer than you think");
+                }
+            }
+        }
+        public Doggy(int hehe, int haha = 100)
+        {
+            HeheScale = hehe;
+            HaHaScale = haha;
+        }
+    }
+
+    internal class NewMemeEventArgs : EventArgs
+    {
+        private readonly string typeOfMemes, MemeFrom;
+
+        public NewMemeEventArgs(string type, string mFrom)
+        {
+            typeOfMemes = type;
+            MemeFrom = mFrom;
+        }
+
+        public string From { get { return MemeFrom; } }
+        public string Meme { get { return "Ha-ha"; } }
+    }
+}
