b71cec12e617fc8ebffc7e4891a81b585e5518e6
Daryayan <daryasa1991@mail.ru>
2019-02-17 22:26:02 +0300

    Domain info

---
+++ DomainGames.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reflection;
+using System.Threading;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main()
+        {
+            AppDomain domain = AppDomain.CurrentDomain;
+            Console.WriteLine("Base Directory: {0}", domain.BaseDirectory);
+            Assembly[] assemblies = domain.GetAssemblies();
+            foreach (Assembly asm in assemblies)
+            {
+                Console.WriteLine(asm.GetName().Name);
+            }
+        }
+
+        public sealed class MonitoringDomain : IDisposable
+        {
+            public AppDomain domain;
+            public TimeSpan ADCpu;
+            public long UseADMemory;
+            public long ADMemory;
+            static MonitoringDomain()
+            {
+                AppDomain.MonitoringIsEnabled = true;
+            }
+
+            public MonitoringDomain(AppDomain ad)
+            {
+                domain = ad ?? AppDomain.CurrentDomain;
+                ADCpu = domain.MonitoringTotalProcessorTime;
+                UseADMemory = domain.MonitoringSurvivedMemorySize;
+                ADMemory = domain.MonitoringTotalAllocatedMemorySize;                    
+            }
+            public void Dispose()
+            {
+                GC.Collect();
+                Console.WriteLine(domain.FriendlyName + (domain.MonitoringTotalProcessorTime
+                    - ADCpu).TotalMilliseconds);
+            }
+        }
+    }
+}
